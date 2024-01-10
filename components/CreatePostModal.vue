dcaf85b685430d2dfdd877217053faacad3c6ce7
Peter Carl Pardo <peterpardo123@gmail.com>
2024-01-10 18:03:01 +0800

    Add file input validation

--- components/CreatePostModal.vue
+++ components/CreatePostModal.vue
@@ -14,30 +14,44 @@
           placeholder="Content here..." />
       </UFormGroup>
 
-      <UFormGroup class="mt-2">
+      <UFormGroup class="my-2">
         <template #label>
           <div
-            class="flex items-center gap-x-2 cursor-pointer hover:bg-gray-100 px-2 py-1 rounded">
+            class="flex flex-col justify-center items-center gap-x-2 cursor-pointer hover:bg-gray-100 px-2 py-1 rounded w-20 h-20">
             <UIcon
-              name="i-mdi-paperclip"
+              name="i-ic-baseline-plus"
               dynamic />
-            Upload File/s
+            <p class="text-xs">Add Image</p>
           </div>
         </template>
 
         <template #default>
           <UInput
             type="file"
-            ref="file"
+            ref="files"
             @change="onChange"
+            multiple
+            class="hidden"
             accept=".jpg,.jpeg,.png" />
         </template>
       </UFormGroup>
 
-      <div v-if="fileDisplay">
-        <img
-          :src="fileDisplay"
-          class="mx-auto w-full mt-2 mr-2 rounded-lg" />
+      <div
+        class="grid grid-cols-2 gap-5"
+        v-if="filesDisplay">
+        <div
+          v-for="(fileDisplay, i) in filesDisplay"
+          :key="i"
+          class="relative grid place-content-center">
+          <img
+            :src="fileDisplay"
+            class="w-full" />
+          <div
+            @click="deleteFile(fileDisplay)"
+            class="absolute inset-0 justify-center flex items-center bg-gray-50 bg-opacity-50 opacity-0 w-full h-full hover:opacity-100 cursor-pointer">
+            <p class="text-red-500 bg-white rounded px-2 py-1">Delete</p>
+          </div>
+        </div>
       </div>
 
       <template #footer>
@@ -61,14 +75,14 @@
 
   const post = ref("");
   const toast = useToast();
-  const file = ref(null);
-  const fileDisplay = ref<string | null>(null);
+  const files = ref(null);
+  const filesDisplay = ref<string[] | null>(null);
   const { isOpen, handleIsOpen } = inject(ModalKey) as Modal;
 
   const isPostValid = computed(() => post.value.length < 20);
 
   const errorMessage = computed(
-    () => !isPostValid.value && "Post must be 20 characters below."
+    () => !isPostValid.value && "Post must be 100 characters below."
   );
 
   const createPost = () => {
@@ -80,7 +94,32 @@
   const onChange = (event: Event) => {
     const el = event.target as HTMLInputElement & EventTarget;
     if (el.files) {
-      fileDisplay.value = URL.createObjectURL(el.files[0]);
+      // Limit file upload to three files only
+      if ((filesDisplay.value as string[])?.length + el.files.length > 3) {
+        alert("Only upload three files");
+        return;
+      }
+
+      let previewImages = [];
+      let fileSize = 0;
+      for (let i = 0; i <= el.files.length - 1; i++) {
+        fileSize += el.files[i].size / 1024 ** 2;
+        previewImages.push(URL.createObjectURL(el.files[i]));
+      }
+
+      // Show alert if file size is greater than 2mb
+      if (fileSize > 2) {
+        alert("Total file size must be less than 2mb only");
+        return;
+      }
+
+      filesDisplay.value = previewImages;
     }
+  };
+
+  const deleteFile = (file: string) => {
+    filesDisplay.value = filesDisplay.value?.filter(
+      (filesDisplay) => filesDisplay !== file
+    ) as string[];
   };
 </script>
