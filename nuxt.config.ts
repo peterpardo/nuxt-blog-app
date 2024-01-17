75659d234b3cfb11fb45763eee0631f80a872141
Peter Carl Pardo <peterpardo123@gmail.com>
2024-01-17 22:34:53 +0800

    Updated apiBase config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiBase: "http://localhost:3000",
+      apiBase: "",
       bucketUrl: process.env.BUCKET_URL,
     },
   },
